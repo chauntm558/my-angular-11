f4c5bd71291a2ab06b4c86c00214774d4bac788c
chauntm558 <chauntm.hpcd@gmail.com>
2021-02-09 09:18:12 +0700

    add new source ver.03

---
+++ my-app/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  url_api: 'https://api.vtvhyundai.vn/front/',
+  imageServer: 'https://image.vtvhyundai.vn',
+  iconServer: 'https://image.vtvhyundai.vn/icon',
+  timeSlide: 2,
+  currentUser: localStorage.getItem("user"),
+};
